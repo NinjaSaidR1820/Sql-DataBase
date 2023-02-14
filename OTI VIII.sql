4b93465270d8c60ce64220914a4c6f25f84dad54
SaidRivas <89547815+NinjaSaidR1820@users.noreply.github.com>
2023-02-14 09:54:01 -0600

    Add files via upload

--- OTI VIII.sql
+++ OTI VIII.sql
@@ -4,7 +4,7 @@ select * from Employees
 select * from Orders
 
 
-create procedure sp_ViewEmpleados
+alter procedure sp_ViewEmpleados
 @fechaI date,
 @FechaF date,
 @Ciudad nvarchar(15)
@@ -26,11 +26,10 @@ select * from Suppliers
 select * from Products
 select * from Categories
 
-Create procedure NProduct
-@PD int,
+alter procedure NProduct
 @NP nvarchar(40),
 @QPU nvarchar(20),
-@Unitprice float,
+@Unitprice money,
 @UnitsInStock int,
 @UnitsInOrder int,
 @ReorderLevel int,
@@ -42,21 +41,8 @@ Create procedure NProduct
 as
 declare @IdSup as int 
 set @IdSup = (select SupplierID from Suppliers where SupplierID=@IDS)
-declare @IdProd as int
-set @IdProd = (select ProductID from Products where ProductID = @PD)
 declare @IdCategory as int 
 set @IdCategory = (select CategoryID from Categories where CategoryID = @IDCat)
-if(@PD='' or @NP='' or @QPU='' or @IDCat='' or @IDS = '')
-begin
- print 'No puede ser Null'
-end
-else
-begin
- if(@PD=@IdProd)
- begin
-  print 'No puede estar duplicado'
- end
- else
  begin
   if(@Unitprice>0)
    begin
@@ -64,7 +50,8 @@ begin
      begin
       if(@IDS=@IdSup)
        begin
-	   insert into Products values(@PD,@NP,@IDS,@IDCat,@QPU,@Unitprice,@UnitsInStock,@UnitsInOrder,@ReorderLevel,0)
+	   insert into Products(ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued)
+	   values(@NP,@IDS,@IDCat,@QPU,@Unitprice,@UnitsInStock,@UnitsInOrder,@ReorderLevel,@Discontinued)
 	   end
 	   else
 	   begin
@@ -81,78 +68,84 @@ begin
  print 'UnitPrice No puede ser menor a 0'
 end
 end
-end
 
 
-NProduct 78,'Galleta',7,4,'44 kg',15.50,100,10,30,0
+
+/*Estructura para Agregar Los Productos
+
+
+ 'ProductName - Quantityper Unit - UnitPrice  -  UnitsInStock - UnitsOnOrder - ReorderLevel - Discount - SupplierID - CategoryID' 
+
+
+*/
+NProduct 'Galleta','54 Und por Caja',20,10,10,10,0,1,2
+
+select * from Products
 
 
 
 --------------------------------------------------------------
-select * from Employees
-select * from Orders
 
 
 
-create procedure sp_Clasificacion
-@fechaI date,
+create procedure Cantidad 
+@FechaI date,
 @FechaF date
 as
-select  E.FirstName,Sum((OD.UnitPrice*OD.Quantity*(1-OD.Discount))) as Ventas,
+select ProductName,Quantity,
 
- Estado =
- CASE
- WHEN sum(OD.UnitPrice*OD.Quantity*(1-OD.Discount)) < 9000 THEN 'Negligente'
- WHEN sum(OD.UnitPrice*OD.Quantity*(1-OD.Discount)) between 9000 and 20000 THEN 'Entrenado'
- WHEN sum(OD.UnitPrice*OD.Quantity*(1-OD.Discount)) > 20000 THEN 'Eficiente'
- else 'No vendio'
- END
+Region=
+case
+when Sup.Region Is Null then 'No Region'
+else
+Sup.Region
+end
+from [Order Details]  OD 
+inner join Orders O on O.OrderID = OD.OrderID
+inner join Products P on P.ProductID = OD.ProductID
+inner join Suppliers Sup on Sup.SupplierID = P.SupplierID
+where O.OrderDate between @FechaI and @FechaF
+group by ProductName,Quantity,Sup.Region
+
+Cantidad '1996-07-03','1996-08-02'
 
 
-from  Employees E
-inner join Orders O on O.EmployeeID = E.EmployeeID
+-------------------------------------------------------------------------
+
+create procedure Clasificacion
+@FechaInicio as date,
+@FechaFinal as date
+as
+Select E.FirstName,SUM((OD.UnitPrice*OD.Quantity*(1-OD.Discount))) as Ventas,
+Estado =
+case 
+when SUM((OD.UnitPrice*OD.Quantity*(1-OD.Discount))) < 9000 then 'Negligente'
+when SUM((OD.UnitPrice*OD.Quantity*(1-OD.Discount))) between 9000 and 20000 then 'Entrenado'
+when SUM((OD.UnitPrice*OD.Quantity*(1-OD.Discount))) > 20000 then 'Eficiente'
+else
+'No Vendio'
+end
+
+
+from Employees E 
+inner join Orders O on E.EmployeeID = O.EmployeeID
 inner join [Order Details] OD on OD.OrderID = O.OrderID
-inner join Products p on p.ProductID = OD.ProductID
-Where O.OrderDate between @fechaI and @FechaF
-
+inner join Products P on P.ProductID = OD.ProductID
+where O.OrderDate between @FechaInicio and @FechaFinal
 group by E.FirstName
-order by E.FirstName 
+order by E.FirstName
 
+Clasificacion '1996-07-03','1997-08-02'
 
-EXEC sp_Clasificacion '1996-01-01','1997-01-01'
 
 ----------------------------------------------------
 
-create procedure CantidadMenosVendida
-@FechaI date, 
-@FechaF date
+
+create procedure ProductMenosVendido
+@Empleado nvarchar(10), @Territory nchar(50)
 as
-select distinct ProductName,Quantity,
-Region =
-case
-when Sp.Region is null then 'Sin Region'
-else
-Sp.Region
-end
-from [Order Details] OD
-inner join Orders O on O.OrderID = OD.OrderID
-inner join Products P on Od.ProductID = P.ProductID
-inner join Suppliers Sp on Sp.SupplierID = P.SupplierID
-where OrderDate between @FechaI and @FechaF
-group by ProductName,Quantity,Sp.Region
-
-
-EXEC CantidadMenosVendida '1995-12-31','1996-12-31'
-
-
------------------------------------------------------------
-
-create procedure Product
-@Nombre nvarchar(50),
-@territory nvarchar(20)
-as
-select top 1 with ties P.ProductName,sum(OD.Quantity) as Cantidad, sum((OD.UnitPrice*OD.Quantity)*(1-OD.Discount)) as Monto
-,T.TerritoryDescription
+select top 1 with ties E.FirstName,P.ProductName,SUM(OD.Quantity) as Cantidad,
+SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount)) as Ventas, T.TerritoryDescription
 
 from Products P 
 inner join [Order Details] OD on OD.ProductID = P.ProductID
@@ -161,11 +154,13 @@ inner join Employees E on E.EmployeeID = O.EmployeeID
 inner join EmployeeTerritories ET on ET.EmployeeID = E.EmployeeID
 inner join Territories T on T.TerritoryID = ET.TerritoryID
 
-where E.FirstName = @Nombre and T.TerritoryDescription = @territory
+
+where E.FirstName = @Empleado and T.TerritoryDescription = @Territory
 group by OD.Quantity,E.FirstName,P.ProductName,T.TerritoryDescription
 order by Cantidad desc
 
-select * from Employees
-select * from Territories
+ProductMenosVendido 'Margaret','Greensboro'
 
-exec Product 'Margaret','Greensboro'
+
+
+-------------------------------------------------------
